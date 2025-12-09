b12a046a25d86b30a89aa2040dc96cd98713001b
nikosrepos2 <designurlife@outlook.com>
2025-12-09 23:32:20 +0100

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Niko",
+    author_email="niko@gmail.com",
+    packages=find_packages() 
+)
+
