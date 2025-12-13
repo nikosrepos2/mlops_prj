a17ba9e349c79280b9d21e748a8e209e5c22370e
nikosrepos2 <designurlife@outlook.com>
2025-12-13 11:20:37 +0100

    Restore local files after OS crash

---
+++ src/components/data_validation.py
@@ -0,0 +1,18 @@
+import  json
+import sys
+import os
+
+import pandas as pd
+
+from pandas import DataFrame
+
+from src.exception import MyException
+from src.logger import logging
+from src.utils.main_utils import read_yaml_file
+from src.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from src.entity.config_entity import DataValidationConfig
+from src.constants import SCHEMA_FILE_PATH
+
+class DataValidation:
+    def __init__(self, data ):
+        
